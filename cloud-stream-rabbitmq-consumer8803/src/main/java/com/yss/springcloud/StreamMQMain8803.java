ab3c93b6df3b1b89d34685e407d9acf788340ad2
duhao <duhao@ysstech.com>
2020-03-24 22:15:26 +0800

    Springcloud Stream 8803 yml中添加group

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/yss/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.yss.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: duhao
+ * @date: 2020/3/24 21:44
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
