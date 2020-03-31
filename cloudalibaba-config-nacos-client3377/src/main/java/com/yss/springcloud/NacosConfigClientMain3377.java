7697db2b6051e737d8d68fd5a706554361373b05
duhao <duhao@ysstech.com>
2020-03-31 11:34:04 +0800

    Springcloud alibaba nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/yss/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.yss.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: duhao
+ * @date: 2020/3/26 21:01
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/yss/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.yss.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: duhao
+ * @date: 2020/3/26 21:03
+ */
+@RestController
+@RefreshScope // 实现配置自动刷新
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
